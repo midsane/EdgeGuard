5116a94cd7a097480208159151df4ac2e3aadafd
midsane <adityaraj10544@gmail.com>
2026-04-30 11:46:06 +0530

    feat: update Redis initialization to include redisOptions with TLS support

--- common/redis.js
+++ common/redis.js
@@ -1,11 +1,18 @@
 import Redis from "ioredis";
 
-const redis = new Redis({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT,
-  maxRetriesPerRequest: 1,
-  enableReadyCheck: true,
-});
+const redis = new Redis(
+  [{
+    host: process.env.REDIS_HOST,
+    port: process.env.REDIS_PORT,
+    maxRetriesPerRequest: 1,
+    enableReadyCheck: true,
+  }],
+  {
+    redisOptions: {
+      tls: {}
+    }
+  }
+);
 
 redis.on("error", (err) => {
   console.error("👀 Redis Error:", err.message);
